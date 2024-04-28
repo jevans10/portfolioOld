6a782a8ad773ec2fec1b39d7e094a461925ac7dc
jevans10 <37997006+jevans10@users.noreply.github.com>
2024-04-27 21:15:54 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,6 +1,12 @@
+// head.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const headElement = document.querySelector('head');
+
   // Fetch and insert the head content
   fetch('head.html')
-  .then(response => response.text())
-  .then(html => {
-    headElement.innerHTML += html;
-  });
+    .then(response => response.text())
+    .then(html => {
+      headElement.innerHTML += html;
+    });
+});
